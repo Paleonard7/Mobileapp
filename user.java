5be72698f5797794dfcfb9a0d55dcceb2e2fc363
leonard <alongsideleo@gmail.com>
2022-03-12 05:46:35 +0000

    Version.2

--- user.java
+++ user.java
@@ -1,4 +1,5 @@
 This is the beginning of the mobile interface development for version 1
 #I am so hyped for this project right now. 
 This is the local repo.
-
+This is the second upgrade 
+I am adding clem
